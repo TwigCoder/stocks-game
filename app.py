a0eaa98815984c17e2f9074d300cc607c18fa8f3
Pranav Verma <pranavverma@Pranavs-MacBook-Air.local>
2025-12-31 15:45:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ from collections import defaultdict
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 from textblob import TextBlob
-import json
 
 conn = sqlite3.connect("stocks_game.db", check_same_thread=False)
 c = conn.cursor()
@@ -138,18 +137,12 @@ def get_stock_data(symbol, period="1y"):
 
 def get_stock_price(stock):
     try:
+        return stock.info["regularMarketPrice"]
+    except (KeyError, TypeError):
         hist = stock.history(period="1d")
         if not hist.empty:
             return hist["Close"].iloc[-1]
-    except Exception as e:
-        print(e)
-
-    try:
-        return stock.info["regularMarketPrice"]
-    except (KeyError, TypeError, json.JSONDecodeError, Exception):
-        pass
-
-    return None
+        return None
 
 
 def get_news(symbol=None, general_market=False):
